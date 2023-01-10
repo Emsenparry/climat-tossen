4e8f207f2736bc26ad1125552c94376086d8f9e8
Emma Nielsen <Emmanielsen843@gmail.com>
2023-01-10 13:59:39 +0100

    scss

--- climate-goals/src/App.jsx
+++ climate-goals/src/App.jsx
@@ -1,6 +1,4 @@
 import './App.scss';
-// SCSS
-// import styles from './components/partials/footer/footer.module.scss'
 
 // Components
 import { Header } from './components/partials/header/header'
@@ -11,7 +9,7 @@ const App = () => {
   return (
     <div>
       <Header />
-      <p>“Alle er vi en del af problemet såvel som af løsningen, og vores medvirken er helt nødvendig, hvis det skal lykkes at få knækket kurven for udslippet af drivhusgasser. Effekten af indsatsen afhænger af, hvor mange der trækker i den rigtige retning.”</p>
+      <p className='subText'>“Alle er vi en del af problemet såvel som af løsningen, og vores medvirken er helt nødvendig, hvis det skal lykkes at få knækket kurven for udslippet af drivhusgasser. Effekten af indsatsen afhænger af, hvor mange der trækker i den rigtige retning.”</p>
       <Main />
       <Footer />
     </div>
